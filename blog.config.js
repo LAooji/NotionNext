68ff92bc0438caf7be3ba0950c28c4e02cc2ef00
LAooji <93930983+LAooji@users.noreply.github.com>
2024-02-05 22:34:12 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '大拙设计', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '大拙空间设计有限公司', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.zhuo.design', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '装修, 大拙, 别墅, 适老化, 养老, 办公, 餐饮, 设计, 装修设计', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '设计,装修,装修设计,室内设计,大拙, 别墅, 适老化, 养老, 办公, 餐饮, 办公室装修,适老化改造,办公室装修,办公室设计,餐饮设计,适老化设计,别墅设计,家装,家装设计', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'zhuo@zhuo.design', // 邮箱地址 例如mail@tangly1024.com
